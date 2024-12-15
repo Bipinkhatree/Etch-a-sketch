78875332edbc68ca1df220da997aa27c9a6b0ac4
Bipin Khatri <159134672+Bipinkhatree@users.noreply.github.com>
2024-12-15 09:02:23 +0545

    Finish setting up functions for creating grids with user input and added eventlisteners to them

--- script.js
+++ script.js
@@ -8,35 +8,59 @@ for (let i = 0; i < 256; i++) {
     container.appendChild(child);
 
 
+
+
+
 };
-
-let childs = document.querySelectorAll(".child");
-
-childs.forEach(child => {
-    child.addEventListener("mouseenter", () => {
-        child.style.backgroundColor = "#FF6347";
-    });
-
-    child.addEventListener("mouseleave", () => {
-        setTimeout(() => {
-            child.style.backgroundColor = 'blueviolet';
-        }, 150);
-
-
-    });
-
-});
-
-
 let gridChangeBtn = document.querySelector("#btn-grid");
+
+function applyEventListeners() {
+    let childs = document.querySelectorAll(".child");
+
+    childs.forEach(child => {
+        child.addEventListener("mouseenter", () => {
+            child.style.backgroundColor = "#FF6347";
+        });
+
+        child.addEventListener("mouseleave", () => {
+            setTimeout(() => {
+                child.style.backgroundColor = 'blueviolet';
+            }, 150);
+        });
+
+    });
+}
+
 
 function removeDivs() {
     container.innerHTML = ''
 };
 
 
+const containerWidth = container.offsetWidth;
+const containerHeight = container.offsetHeight;
+
 gridChangeBtn.addEventListener("click", () => {
-    removeDivs();
+
+    let noOfGrids = prompt("Please enter the number of grids less than or equal to 100...");
+    if (noOfGrids > 100) {
+        prompt("Please ensure you have entered less than or equal to 100....")
+    } else {
+        removeDivs();
+        for (let i = 0; i < (noOfGrids * noOfGrids); i++) {
+            let child = document.createElement('div');
+            child.classList.add("child");
+            container.appendChild(child);
+            const childSize = Math.min(containerWidth, containerHeight) / noOfGrids - 5;
+            child.style.width = `${childSize}px`;
+            child.style.height = `${childSize}px`;
+
+            applyEventListeners();
 
 
+        }
+    }
 })
+
+
+applyEventListeners();
