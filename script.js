b85e43cbf8fef9b09c1d7ba0a2f52438f578073d
Bipin Khatri <159134672+Bipinkhatree@users.noreply.github.com>
2024-12-15 07:37:43 +0545

    Finish building 16 * 16 square grid box and Finish adding hover effect to them

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 let container = document.querySelector("#container");
+
+
+
+for (let i = 0; i < 256; i++) {
+    let child = document.createElement("div");
+    child.classList.add("child");
+    container.appendChild(child);
+
+
+};
+
+let childs = document.querySelectorAll(".child");
+
+childs.forEach(child => {
+    child.addEventListener("mouseenter", () => {
+        child.style.backgroundColor = "#FF6347";
+    });
+
+    child.addEventListener("mouseleave", () => {
+        setTimeout(() => {
+            child.style.backgroundColor = 'blueviolet';
+        }, 150);
+
+
+    });
+
+});
+
+
+let gridChangeBtn = document.querySelector("#btn-grid");
+
+function removeDivs() {
+    container.innerHTML = ''
+};
+
+
+gridChangeBtn.addEventListener("click", () => {
+    removeDivs();
+
+
+})
